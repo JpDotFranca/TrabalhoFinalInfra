8d9fd6473a435924824f498c04ce2b69c7312929
João Paulo Ferreira <joao@safe2pay.com.br>
2020-12-01 20:34:31 -0300

    up

--- API/Controllers/ImageController.cs
+++ API/Controllers/ImageController.cs
@@ -12,7 +12,7 @@ namespace API.Controllers
         {
             Uploader.UploadImage(obj.base64Image);
 
-            return Ok(); 
+            return Ok("Imagem processada e armazenada com sucesso!"); 
         }
 
         public class Payload
